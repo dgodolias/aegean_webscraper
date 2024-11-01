f2ffec17657ec126d8bf1bdb185c720e707f0ba7
dgodolias <dgodolias@gmail.com>
2024-11-01 15:46:26 +0200

    updates

--- aegean_scraper.py
+++ aegean_scraper.py
@@ -30,8 +30,8 @@ def init_driver(thread_id):
     chrome_options.add_argument(f"user-data-dir={user_data_dir}")
     
     chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("--enable-unsafe-webgl")  # Enable WebGL in headless mode
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
@@ -40,14 +40,12 @@ def init_driver(thread_id):
     chrome_options.add_argument("--disable-popup-blocking")
     chrome_options.add_argument("--disable-infobars")
     chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--window-position=-10000,-10000")  # Move window off-screen
 
     # Add a normal user agent string
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     )
-
-    # Add this option to handle rendering differences in headless mode
-    chrome_options.add_argument("--window-size=1920,1080")
 
     return webdriver.Chrome(options=chrome_options)
 
